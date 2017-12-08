0b022074e6f93a7659a19be424ed36d833c7d497
xuyuanfan77 <xuyuanfan77@outlook.com>
2017-12-08 17:33:16 +0800

    增加导出功能（未完成）

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -179,24 +179,12 @@ class UserController extends Controller {
 		
 		$userList = $user->where($condition)->order('create_date desc')->page($page.','.$rows)->select(); */
 		
-		include 'PHPExcel.php';
-		include 'PHPExcel/Writer/Excel2007.php';
-		//或者include 'PHPExcel/Writer/Excel5.php'; 用于输出.xls的
-		创建一个excel
-		$objPHPExcel = new PHPExcel();
-		//输出Excel表格到浏览器下载
-		header('Content-Type: application/vnd.ms-excel');
-		header('Content-Disposition: attachment;filename="abc.xls"');
-		header('Cache-Control: max-age=0');
-		// If you're serving to IE 9, then the following may be needed
-		header('Cache-Control: max-age=1');
-		// If you're serving to IE over SSL, then the following may be needed
-		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-		header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
-		header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-		header('Pragma: public'); // HTTP/1.0
+		import("Org.Util.PHPExcel");
+        import("Org.Util.PHPExcel.Writer.Excel2007");
+		
+		$objPHPExcel = new \PHPExcel();
 		$objWriter = new PHPExcel_Writer_Excel5($objPHPExcel);
-		$objWriter->save('php://output');
+		$objWriter->save('output');
 		
 		
 		$result = json_encode(array('errorMsg' => $_POST['sName']));
